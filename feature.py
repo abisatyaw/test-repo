2aef2d731b8c180e7150e915d3265b5a79cca8d1
Abisatya <abisatya90@gmail.com>
2025-04-21 14:09:43 +0700

    Dev to C (#23)
    * feat 1 first
    
    * feat1 2nd commit
    
    * feat1 third commit
    
    * Feat 2 Added (#21)
    
    * feat2 first commit
    
    * feat2 second commit
    
    * feat2 third commit

--- feature.py
+++ feature.py
@@ -1,5 +1,13 @@
 print("feat_a_first commit")
 
+print("feat1 first commit")
+print("feat1 second commit")
+print("feat1 third commit")
+
+print("feat2 first commit")
+print("feat2 second commit")
+print("feat2 third commit")
+
 print("feat3_first commit")
 print("feat3_second commit")
 print("feat3_third commit")
